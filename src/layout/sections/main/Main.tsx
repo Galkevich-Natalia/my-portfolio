1cb8880cc85bfd24cbbc66adc1586e56668a8228
Galkevich-Natalia <galkevich.natalia@gmail.com>
2026-04-22 00:15:13 +0300

    created Main section structure

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import photo from "./../../../assets/images/photo.webp";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Main = () => {
+  return (
+    <StyledMain>
+      <FlexWrapper justify={"space-around"} align={"center"}>
+        <div>
+          <span>Hi everybody!</span>
+          <Name>I'm Natalia Galkevich</Name>
+          <MainTitle>A web developer</MainTitle>
+        </div>
+
+        <Photo src={photo} alt="" />
+      </FlexWrapper>
+    </StyledMain>
+  );
+};
+
+const StyledMain = styled.div`
+  /* min-height: 100vh; */
+  background-color: #fcdc67;
+`;
+
+const Photo = styled.img`
+  width: 350px;
+  height: 350px;
+  object-fit: cover;
+`;
+
+const MainTitle = styled.h1``;
+
+const Name = styled.h2``;
